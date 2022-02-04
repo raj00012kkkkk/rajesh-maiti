ebcf87a508441e568a795aa4d66b816888e401d5
rajesh <rajeshmaiti1606@gmail.com>
2022-02-04 15:28:23 +0530

    f

---
+++ practical/Arraysort.java
@@ -0,0 +1,21 @@
+package practical;
+import java.util.Arrays;
+
+
+public class Arraysort{
+
+	public static void main(String[] args) {
+		//defining an array of integer type   
+		int [] a= new int [] {90, 23, 5, 109, 12, 22, 67, 34};  
+		//invoking sort() method of the Arrays class  
+		Arrays.sort(a);   
+		System.out.println("Elements of array sorted in ascending order: ");  
+		//prints array using the for loop  
+		for (int i = 0; i < a.length; i++)   
+		{       
+		System.out.print(a[i]+ " ");   
+		}
+
+	}
+
+}
